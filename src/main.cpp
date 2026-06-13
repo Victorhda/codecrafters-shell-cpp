ba8b3e315fffb57e7b8999959308110125b1055a
Victor Alves <80107777+Victorhda@users.noreply.github.com>
2026-06-13 12:07:24 +0200

    codecrafters submit [skip ci]

--- src/main.cpp
+++ src/main.cpp
@@ -7,12 +7,22 @@ int main() {
   std::cout << std::unitbuf;
   std::cerr << std::unitbuf;
 
-  std::cout << "$ ";
   
-  // Get user input as string
-  std::string input;
-  std::cin >> input;
 
-  // Output command not found
-  std::cout << input << ": command not found";
+  bool is_running = true;
+  
+  while (is_running)
+  {
+    std::cout << "$ ";
+
+    // Get user input as string
+    std::string input;
+    std::cin >> input;
+
+    // Output command not found
+    std::cout << input << ": command not found";
+    
+    std::cout << "\n";
+  }
+  
 }
