36cf7fcab42892d38f86147b147e42c67743f4ad
Victor Alves <80107777+Victorhda@users.noreply.github.com>
2026-06-13 12:00:37 +0200

    codecrafters submit [skip ci]

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <string>
 
+
 int main() {
   // Flush after every std::cout / std:cerr
   std::cout << std::unitbuf;
@@ -8,4 +9,10 @@ int main() {
 
   // TODO: Uncomment the code below to pass the first stage
   std::cout << "$ ";
+  
+  std::string input;
+
+  std::cin >> input;
+
+  std::cout << "{" << input << "}: command not found";
 }
